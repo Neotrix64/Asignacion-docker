697b37cf94f88e075bb0c16c22816d8d5b4baec7
gustavo <gustavo_angel27@hotmail.com>
2024-03-17 18:50:41 -0400

    first commit

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicación PHP Docker</title>
+</head>
+<body>
+    <h1>Bienvenido a mi aplicación PHP Docker</h1>
+    <p>Gustavo Ramirez 2022-2019</p>
+</body>
+</html>
+
+<?php
